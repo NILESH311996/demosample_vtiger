c99fb64697f8841dbe1c319e0281aa0689bdc749
Neeraj <Neeraj@DESKTOP-DD6SCUV>
2025-12-30 14:57:05 +0530

    gcch

--- src/test/java/Leads/CreateLeadsPage.java
+++ src/test/java/Leads/CreateLeadsPage.java
@@ -10,10 +10,8 @@ public void test1()
 {
 	System.out.println("======PRINT LEADS PAGE--------");
 	System.out.println("----changes done---");
-<<<<<<< HEAD
+
 	System.out.println(" --------resolving git conflict--------");
-=======
 	System.out.println("pooja statement");
->>>>>>> branch 'master' of https://github.com/NILESH311996/demosample_vtiger.git
 }
 }
